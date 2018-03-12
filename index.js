c4b8e8b5bc6824590d1f6818e8b850f59c530025
hungphamuy95 <hungpham.huy95@gmail.com>
2018-03-12 13:53:44 +0700

    enable cors middleware

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const iprequest = require('request-ip');
-
+const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const config = require('./config');
 const User = require('./app/models/users');
@@ -15,26 +15,9 @@ mongoose.connect(config.localUrl);
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-
+app.use(cors());
 app.use(morgan('dev'));
-app.use((req, res, next) => {
-    
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', '*');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    //res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-})
 const apiRouters= express.Router();
 
 require('./app/controllers/theardController')(apiRouters);
